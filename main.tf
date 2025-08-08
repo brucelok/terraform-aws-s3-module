891eef9845a07c9b0475442567d65a7b68dc22d5
Bruce L. <2253713+brucelok@users.noreply.github.com>
2025-08-08 10:52:55 +1000

    add bucket variables

--- main.tf
+++ main.tf
@@ -7,10 +7,19 @@ terraform {
   }
 }
 
+variable "bucket_name" {
+  type        = string
+  description = "S3 bucket name please"
+}
+
 provider "aws" {
   region = "ap-southeast-2"
 }
 
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
+}
+
+output "bucket_arn" {
+  value = aws_s3_bucket.this.arn
 }
